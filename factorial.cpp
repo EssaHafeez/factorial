2cd1d4c8540edcb00aaedfc3c33c87e7246369ee
EssaHafeez <107763374+EssaHafeez@users.noreply.github.com>
2022-07-29 12:55:00 -0700

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int factorial =1;
+
+
+for(int i=1;i<=5;i++){
+
+factorial=factorial*i;
+cout<<"factorial of 5 :"<<factorial<<endl;
+}
+    return 0;
+}
